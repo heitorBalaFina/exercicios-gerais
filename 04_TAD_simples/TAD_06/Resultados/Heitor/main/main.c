b413c9670b00bd37a18443aeecf2849bc73a5591
Heitor Rudio Vichelo <150042013+heitorBalaFina@users.noreply.github.com>
2026-09-08 13:37:41 +0000

    mais progresso

--- 04_TAD_simples/TAD_06/Resultados/Heitor/main/main.c
+++ 04_TAD_simples/TAD_06/Resultados/Heitor/main/main.c
@@ -11,7 +11,55 @@ int main(){
     tMatrix matrix2 = MatrixCreate(linhas, colunas);
     matrix2 = MatrixRead(matrix2);
 
-    int escolha, escalar, opcao;
+    int escolha, escalar, opcao = 0;
+    while(opcao != 6){
+        printf("1 - Somar matrizes\n");
+        printf("2 - Subtrair matrizes\n");
+        printf("3 - Multiplicar matrizes\n");
+        printf("4 - Multiplicacao de uma matriz por escalar\n");
+        printf("5 - Transposta de uma matriz\n");
+        printf("6 - Encerrar o programa\n");
+        printf("Opcao escolhida: \n");
+        scanf("%d", &escolha);
+        switch (escolha){
+        case 1:
+           if(PossibleMatrixSum(matrix1, matrix2)){
+                MatrixPrint(MatrixAdd(matrix1, matrix2));
+            }
+            break;
+        case 2:
+            if(PossibleMatrixSub(matrix1, matrix2)){
+                MatrixPrint(MatrixSub(matrix1, matrix2));
+            }
+            break;
+        case 3:
+            if(PossibleMatrixMultiply(matrix1, matrix2)){
+                MatrixPrint(MatrixMultiply(matrix1, matrix2));
+            }
+            break;
+        case 4:
+            scanf("%d %d", &escalar, &opcao);
+            if(opcao == 1){
+                matrix1 = MatrixMultiplyByScalar(matrix1, escalar);
+                MatrixPrint(matrix1);
+            } else if(opcao == 2){
+                matrix2 = MatrixMultiplyByScalar(matrix2, escalar);
+                MatrixPrint(matrix2);
+            }
+            break;
+        case 5:
+            scanf("%d", &opcao);
+            if(opcao == 1 || opcao == 2){
+                MatrixPrint(TransposeMatrix(matrix1));
+                MatrixPrint(TransposeMatrix(matrix2));
+            }
+            break;
+        default:
+            return 0;
+            break;
+        }
+    }
+    /*
     do{
         printf("1 - Somar matrizes\n");
         printf("2 - Subtrair matrizes\n");
@@ -51,16 +99,15 @@ int main(){
             break;
         case 5:
             scanf("%d", &opcao);
-            if(opcao == 1){
+            if(opcao == 1 || opcao == 2){
                 MatrixPrint(TransposeMatrix(matrix1));
-                MatrixPrint(matrix2);
-            } else if(opcao == 2){
-                MatrixPrint(matrix1);
                 MatrixPrint(TransposeMatrix(matrix2));
             }
             break;
         }
     } while (escolha != 6);
+    */
+    
     
     return 0;
 }
