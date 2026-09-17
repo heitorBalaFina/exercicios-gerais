12ac7b16d6ed59069ad4b4c050c6affdcbdf330c
Heitor Rudio Vichelo <150042013+heitorBalaFina@users.noreply.github.com>
2026-09-17 16:53:46 +0000

    comeco do ponteiro

---
+++ 05_ponteiros/pont_01/Respostas/Heitor/utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+int TrocaInterios(int * n1, int * n2){
+    if(n1 >= n2){
+        int troca = n1;
+        n1 = n2;
+        n2 = troca;
+        return 1;
+    }
+    return 0;
+}
